a11fb103586f1a057890d91638e5b7a89c4af27f
faudi <alexnagornyi17@gmail.com>
2022-06-26 23:55:54 +0300

    верстка

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import Header from './components/Header';
+import Card from "./components/Card";
 function App() {
+
+  const items = [
+    {"name": "ux", "price": "3 286 000", "imgUrl": "./img/ux.png", "id": 1},
+    {"name": "rx", "price": "5 348 000", "imgUrl": "./img/rx.png", "id": 2},
+    {"name": "ls", "price": "9 682 000", "imgUrl": "./img/ls.png", "id": 3},
+    {"name": "lc", "price": "12 113 000", "imgUrl": "./img/lc.png", "id": 4},
+    {"name": "es", "price": "4 314 000", "imgUrl": "./img/es.png", "id": 5},
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="searchList">
+        <div className="searchList__item">
+          <p>Все</p>
+        </div>
+        <div className="searchList__item">
+          <p>Седаны</p>
+        </div>
+        <div className="searchList__item">
+          <p>Купе</p>
+        </div>
+        <div className="searchList__item">
+          <p>Гибрид</p>
+        </div>
+      </div>
+      <h2 className="content__title">Все модели</h2>
+      <div className="content__list">
+        {items.map((obj) => (
+          <Card title={obj.name}
+                key={obj.key}
+                price={obj.price}
+                imgUrl={obj.imgUrl}
+          />
+        ))}
+      </div>
+     
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = ({title,price, imgUrl}) => {
+  return (
+    <div className="card">
+      <h3 className="card__title">{title}</h3>
+      <img width={300} className="card__image" src={imgUrl} />
+      <div className="card__wrapper">
+        <p className="card__price">Цена от {price} руб</p>
+        <div className="add-btn">Добавить +</div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
